508cc5a32bb90853056d7cb1d9e2b2c13c702a56
NathanD4vid <nathan.silva@sptech.school>
2022-11-24 11:16:03 -0300

    corrigido erro no questionário

--- Telas e API/site/src/database/config.js
+++ Telas e API/site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "overwatch",
-    user: "overwatch",
-    password: "over10",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
